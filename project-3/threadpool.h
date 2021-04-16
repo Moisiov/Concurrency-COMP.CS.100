829e2c2d7c598ffc94a7a84c574abff0f9c60b35
Valto Moisio <valto.moisio@tuni.fi>
2021-04-16 15:27:39 +0300

    Add basic structure to threadpool.h

--- project-3/threadpool.h
+++ project-3/threadpool.h
@@ -1,11 +1,25 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
+#include <functional>
+#include <thread>
+#include <vector>
+#include <condition_variable>
+#include <mutex>
 
 class ThreadPool
 {
 public:
-    ThreadPool();
+    using Task = std::function<void()>;
+
+    explicit ThreadPool(std::size_t threadCount);
+
+    ~ThreadPool();
+
+private:
+    std::vector<std::thread> m_threads_;
+    std::condition_variable m_cond_var_;
+    std::mutex m_mutex_;
 };
 
 #endif // THREADPOOL_H
