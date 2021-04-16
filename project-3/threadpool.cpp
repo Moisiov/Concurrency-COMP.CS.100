a9062e9d2e3378d47d43989849e0a82608f653a0
Valto Moisio <valto.moisio@tuni.fi>
2021-04-16 16:03:24 +0300

    Add function definitions in ThreadPool class

--- project-3/threadpool.cpp
+++ project-3/threadpool.cpp
@@ -2,5 +2,32 @@
 
 ThreadPool::ThreadPool()
 {
+    unsigned supported_threads = std::thread::hardware_concurrency();
+    start(supported_threads ? supported_threads : 8);
+}
 
+ThreadPool::ThreadPool(std::size_t thread_count)
+{
+    start(thread_count);
+}
+
+ThreadPool::~ThreadPool()
+{
+    stop();
+}
+
+void ThreadPool::start(std::size_t thread_count)
+{
+    for (unsigned i = 0; i < thread_count; ++i)
+    {
+        m_threads_.emplace_back([=]{
+            while (1)
+            {
+                std::unique_lock<std::mutex> lock{m_mutex_};
+                m_cond_var_.wait(lock, [=]{return m_stopping_;});
+
+                if (m_stopping_) break;
+            }
+        });
+    }
 }
--- project-3/threadpool.h
+++ project-3/threadpool.h
@@ -16,6 +16,19 @@ class ThreadPool
 public:
     using Task = std::function<void()>;
 
+    /**
+     * @brief ThreadPool constructor creates a ThreadPool object.
+     * Constructor checks the defined amount of threads in the system
+     * and adds that many threads to the pool. If supported threads
+     * is not defined, number of threads defaults to 8.
+     */
+    ThreadPool();
+
+    /**
+     * @brief ThreadPool constructor overload allows ThreadPool to be
+     * created with any given number of threads.
+     * @param thread_count number of threads
+     */
     explicit ThreadPool(std::size_t thread_count);
 
     ~ThreadPool();
@@ -28,7 +41,7 @@ private:
     std::queue<Task> m_tasks_;
 
     void start(std::size_t thread_count);
-    void stop();
+    void stop() noexcept;
 };
 
 #endif // THREADPOOL_H
