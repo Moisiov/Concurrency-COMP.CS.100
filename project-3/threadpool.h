e8605c4f5ab9b38f9cd826e0e20f9385c8df7875
Valto Moisio <valto.moisio@tuni.fi>
2021-04-16 15:32:30 +0300

    Add member variables and function declarations in threadpool.h

--- project-3/threadpool.h
+++ project-3/threadpool.h
@@ -6,13 +6,17 @@
 #include <vector>
 #include <condition_variable>
 #include <mutex>
+#include <queue>
 
+/**
+ * @brief The ThreadPool class handles threads and tasks.
+ */
 class ThreadPool
 {
 public:
     using Task = std::function<void()>;
 
-    explicit ThreadPool(std::size_t threadCount);
+    explicit ThreadPool(std::size_t thread_count);
 
     ~ThreadPool();
 
@@ -20,6 +24,11 @@ private:
     std::vector<std::thread> m_threads_;
     std::condition_variable m_cond_var_;
     std::mutex m_mutex_;
+    bool m_stopping_ = false;
+    std::queue<Task> m_tasks_;
+
+    void start(std::size_t thread_count);
+    void stop();
 };
 
 #endif // THREADPOOL_H
